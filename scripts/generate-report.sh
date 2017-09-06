74de63bb622b63a15af67c75c8829bf2f4681c73
Philip Tzou <philiptz@stanford.edu>
2017-09-06 13:07:33 -0700

    Change report.md to README.md

--- scripts/generate-report.sh
+++ scripts/generate-report.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /app/report
 Rscript /app/scripts/make-graphical-summary.r
-python3 /app/scripts/generate-report.py > /app/report/report.md
+python3 /app/scripts/generate-report.py > /app/report/README.md
